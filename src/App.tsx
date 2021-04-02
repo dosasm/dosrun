32e9de34f0b3904c88c7de49bc88270a73991ba8
Chen Leo <xsro@foxmail.com>
2021-04-02 23:25:37 +0800

    :art: align two pannel according to screen size

--- src/App.tsx
+++ src/App.tsx
@@ -13,23 +13,32 @@ class App extends React.Component {
   }
 
   renderDosPlayer() {
-    return <JSDos onGetfs={fs=>{(window as any).fs=fs}}/>;
+    return (
+      <JSDos
+        onGetfs={(fs) => {
+          (window as any).fs = fs;
+        }}
+      />
+    );
   }
   renderEditor() {
-    return <EditPanel/>;
+    return <EditPanel />;
   }
   render() {
+    const { height, width } = window.screen;
+    const styles =
+      height > width
+        ? [{ width: "100%" }, { width: "100%" }]
+        : [
+            { float: "left", width: "50%" },
+            { float: "right", width: "50%", height: "100%" },
+          ];
     return (
       <div>
-        <div
-          className="Dosbox"
-          style={{ float: "left", width: "50%" }}
-        >
+        <div className="Dosbox" style={styles[0]}>
           {this.renderDosPlayer()}
         </div>
-        <div style={{ float: "right", width: "50%", height: "100%" }}>
-          {this.renderEditor()}
-        </div>
+        <div style={styles[1]}>{this.renderEditor()}</div>
       </div>
     );
   }
